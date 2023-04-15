8b8a5b1bab767521a1cc9c3c08b433379d380b38
Matthew Bingham <mb54843@gmail.com>
2023-04-15 17:15:17 -0600

    Added movement.  Not sure if this is complete but here you go.

--- Galaga/Player.cs
+++ Galaga/Player.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +10,24 @@ namespace Galaga.Galaga
 {
     class Player
     {
-        int x , y;
+        int x, y;
         int xSize, ySize;
         int scale;
 
         public Player()
         {
             
+        }
+        public void movement() 
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Left)) 
+            {
+                x -= 1;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right)) 
+            {
+                x += 1;
+            }
         }
     }
 }
