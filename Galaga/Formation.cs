181340d0f883b95bff721099d37eb7dba245951d
Kelton Chenworth <kchenworth@gmail.com>
2023-04-25 23:18:26 -0600

    REmoved merge conflict

--- Galaga/Formation.cs
+++ Galaga/Formation.cs
@@ -357,7 +357,6 @@ namespace Galaga.Galaga
             List<Enemy> list;
             formation.Add(new List<Enemy>());
             list = formation[0];
-<<<<<<< HEAD
             for (int i = 0; i < 4; i++)
             {
                 list.Add(new Bee(gameInfo, 10, i * 100));
@@ -382,49 +381,6 @@ namespace Galaga.Galaga
             for (int i = 0; i < 8; i++)
             {
                 list.Add(new Bee(gameInfo, 12, i * 100 + 14000));
-=======
-            list.Add(new EmptyEnemy());
-            list.Add(new EmptyEnemy());
-            list.Add(new EmptyEnemy());
-            list.Add(new Boss(gameInfo, 2, 3000));
-            list.Add(new Boss(gameInfo, 2, 3400));
-            list.Add(new Boss(gameInfo, 2, 3600));
-            list.Add(new Boss(gameInfo, 2, 3200));
-            list.Add(new EmptyEnemy());
-            list.Add(new EmptyEnemy());
-            list.Add(new EmptyEnemy());
-
-            for (int i = 0; i < 2; i++)
-            {
-                formation.Add(new List<Enemy>());
-                list = formation[i + 1];
-                list.Add(new EmptyEnemy());
-                list.Add(new Butterfly(gameInfo, 3, 5000 + i * 400));
-                list.Add(new Butterfly(gameInfo, 3, 5200 + i * 400));
-                list.Add(new Butterfly(gameInfo, 2, 3100 + i * 400));
-                list.Add(new Butterfly(gameInfo, 1, 0 + i * 200));
-                list.Add(new Butterfly(gameInfo, 1, 100 + i * 200));
-                list.Add(new Butterfly(gameInfo, 2, 3300 + i * 400));
-                list.Add(new Butterfly(gameInfo, 3, 5300 + i * 400));
-                list.Add(new Butterfly(gameInfo, 3, 5100 + i * 400));
-                list.Add(new EmptyEnemy());
-            }
-
-            for (int i = 0; i < 3; i++)
-            {
-                formation.Add(new List<Enemy>());
-                list = formation[i + 3];
-                list.Add(new Bee(gameInfo, 1, 9000 + i * 400));
-                list.Add(new Bee(gameInfo, 1, 9200 + i * 400));
-                list.Add(new Bee(gameInfo, 0, 7000 + i * 400));
-                list.Add(new Bee(gameInfo, 0, 7200 + i * 400));
-                list.Add(new Bee(gameInfo, 0, 0 + i * 200));
-                list.Add(new Bee(gameInfo, 0, 100 + i * 200));
-                list.Add(new Bee(gameInfo, 0, 7300 + i * 400));
-                list.Add(new Bee(gameInfo, 0, 7100 + i * 400));
-                list.Add(new Bee(gameInfo, 1, 9300 + i * 400));
-                list.Add(new Bee(gameInfo, 1, 9100 + i * 400));
->>>>>>> 8e0b2ba3fab39289e34b766b16fec4039d6096b1
             }
         }
     }
