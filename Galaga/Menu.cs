81b9bce92b869170e370d1bbabbfe759c52334aa
Mscrib <mattscribner45@gmail.com>
2023-04-25 23:48:55 -0600

    Fixed accuracy logic

--- Galaga/Menu.cs
+++ Galaga/Menu.cs
@@ -300,7 +300,7 @@ namespace Galaga.Galaga
             m_spriteBatch.DrawString(smallEl, "Shots Hit: " + gameInfo.hits, new Vector2(WIDTH / 4, HEIGHT * 9 / 16), Color.White);
             if (gameInfo.hits != 0)
             {
-                m_spriteBatch.DrawString(smallEl, "Accuracy: " + (float)gameInfo.shotsFired / (float)gameInfo.hits, new Vector2(WIDTH / 4, HEIGHT * 10 / 16), Color.White);
+                m_spriteBatch.DrawString(smallEl, "Accuracy: " + (float)gameInfo.hits / (float)gameInfo.shotsFired, new Vector2(WIDTH / 4, HEIGHT * 10 / 16), Color.White);
             }
             m_spriteBatch.DrawString(smallEl, "Press Space to return to menu", new Vector2(WIDTH / 4, HEIGHT * 3 / 4), Color.White);
 
